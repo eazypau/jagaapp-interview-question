335a673d1e6dfa27da2bae07080487201e695cf7
Po YZ <poyizhi@gmail.com>
2023-04-21 17:17:24 +0800

    chore: import Icon Button and Nested Accordion into index file

---
+++ components/index.js
@@ -0,0 +1,5 @@
+import NestedAccordion from "./NestedAccordion";
+import ExampleNestedAccordion from "./ExampleNestedAccordion";
+import IconButtonComponent from "./IconButtonComponent";
+
+export { NestedAccordion, ExampleNestedAccordion, IconButtonComponent };

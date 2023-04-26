f50d8b9354e38eeffef89a4213e52304da94bf46
Po YZ <poyizhi@gmail.com>
2023-04-26 21:38:45 +0800

    chore: shift example data in a new file and directory

---
+++ sample data/exampleData.js
@@ -0,0 +1,104 @@
+const exampleData = [
+	{
+		title: "Important List",
+		description: "",
+		items: [
+			{
+				title: "Book a restaurant",
+				description: "Remember book a restaurant at 6th of December",
+				items: [
+					{
+						title: "Dishes",
+						description: "",
+						items: [
+							{
+								title: "Fried Vegetable",
+								description: "6pax",
+							},
+							{
+								title: "Fried Chicken",
+								description: "4pax",
+							},
+						],
+					},
+				],
+			},
+			{
+				title: "Restock groceries",
+				description: "Get the butter",
+			},
+		],
+	},
+	{
+		title: "Urgent",
+		description: "",
+		items: [
+			{
+				title: "Fetch kids",
+				description: "Today 5pm",
+			},
+		],
+	},
+];
+
+const exampleData2 = [
+	{
+		title: "Important List",
+		description: "",
+		items: [
+			{
+				title: "Restock groceries",
+				description: "Get the butter",
+			},
+		],
+	},
+	{
+		title: "Urgent",
+		description: "",
+		items: [
+			{
+				title: "Fetch kids",
+				description: "Today 5pm",
+			},
+		],
+	},
+];
+
+const exampleData3 = [
+	{
+		title: "Important List",
+		description: "",
+		items: [
+			{
+				title: "Dishes",
+				description: "",
+				items: [
+					{
+						title: "Fried Vegetable",
+						description: "6pax",
+					},
+					{
+						title: "Fried Chicken",
+						description: "4pax",
+					},
+				],
+			},
+			{
+				title: "Restock groceries",
+				description: "Get the butter",
+			},
+		],
+	},
+	{
+		title: "Urgent",
+		description: "",
+		items: [
+			{
+				title: "Fetch kids",
+				description: "Today 5pm",
+			},
+		],
+	},
+];
+
+export { exampleData, exampleData2, exampleData3 };
